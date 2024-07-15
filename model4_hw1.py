6547faeb68f0f0addf30bc215ba0835410d81010
IAbramkina <sinbellezza@gmail.com>
2024-07-15 15:25:16 +0200

    task_1

---
+++ model4_hw1.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+def total_salary(path):
+    try:  
+        lines = []
+        total_salary = 0
+        average_salary = 0
+        count = 0
+
+        with open(path, "rt", encoding='utf-8') as fh:
+            for el in fh.readlines():
+                if el.find(",")>-1:
+                    try:
+                        total_salary +=int(el.strip().split(',').pop(1))
+                    except ValueError as e:
+                        print(e)
+                        continue
+                    count +=1
+                else:
+                    continue
+                
+            try:    
+                average_salary = int(total_salary/count)
+            except ZeroDivisionError as e:
+                print(e)
+
+            lines.append(total_salary)
+            lines.append(average_salary)
+            lines = tuple(lines)
+        return lines
+    except OSError:
+        print('Error while reading File') 
+print(total_salary("text.txt"))
